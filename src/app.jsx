3f86bbd51f3cc90ed75ba9e65506a17e534655ad
Alec Davis <unlikelytitan@gmail.com>
2026-02-12 10:10:03 -0700

    moved more files around and stubbed out jsx files

--- src/app.jsx
+++ src/app.jsx
@@ -2,5 +2,23 @@ import React from 'react';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-dark text-light">
+        <header>
+            <h1 className="mb-4 text-4xl font-bold tracking-tight text-heading md:text-5xl lg:text-6xl">Startup</h1>
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="user.html">📬 UserName</a>
+            </nav>
+        </header>
+
+        <main>App components go here</main>
+
+        <footer>
+            <span class="text-reset">Alec Davis</span>
+            <br />
+            <a href="https://github.com/Ki11erRabbit/startup">GitHub</a>
+        </footer>
+    </div>
+  );
 }
