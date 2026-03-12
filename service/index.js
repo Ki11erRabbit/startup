4f0805ab8f63ede7b2ba2836a7b9a316e58b7d12
Alec Davis <unlikelytitan@gmail.com>
2026-03-12 14:24:07 -0600

    added apis for signing up, logging in, logging out, creating a post, and getting a board's posts

--- service/index.js
+++ service/index.js
@@ -18,11 +18,13 @@ let users = [];
 
 async function createUser(email, password) {
     const passwordHash = await bcrypt.hash(password, 10);
+    const user_id = users.length;
 
     const user = {
         email: email,
         password: passwordHash,
         token: uuid.v4(),
+        user_id: user_id,
     };
     users.push(user);
     return user;
@@ -78,8 +80,65 @@ async function getPosts(board) {
 }
 
 apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+        res.status(409).send({ msg: "Existing user" })
+    } else {
+        const user = createUser(req.body.email, req.body.password);
 
-})
+        setAuthCookie(res, user.token);
+        res.send({ email: user.email });
+    }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
+
+// Get a board's posts
+apiRouter.get('/:board', async (req, res) => {
+    const board = req.params["board"];
+    const posts = await getPosts(board);
+
+    res.send(posts);
+
+});
+
+// Create a post for a board
+apiRouter.post('/:board', verifyAuth, async (req, res) => {
+    const board = req.params["board"];
+    await createPost(req.body.user_id, board, req.body.title, req.body.body_text, req.body.image, req.body.reply_id);
+
+    res.status(200).send();
+
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
