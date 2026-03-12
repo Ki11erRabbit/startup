dba78eb5751829feed12f9481fc7362327e8b00e
Alec Davis <unlikelytitan@gmail.com>
2026-03-12 13:53:12 -0600

    initial commit for service

---
+++ service/index.js
@@ -0,0 +1,15 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
